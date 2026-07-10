8c55e1b3db8dbac79df19630347abc1b56e8cff3
Musarrat Jahan <jahan786musarrat.786@gmail.com>
2026-07-10 19:36:32 +0530

    POJO class

---
+++ 08_Classes/src/Pojo_class/Student.java
@@ -0,0 +1,34 @@
+package Pojo_class;
+
+public class Student {
+    String name;
+    private int rollno;
+    protected String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+}
