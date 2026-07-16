802edd0220b063873afc002e722e2c50a6222672
Musarrat Jahan <jahan786musarrat.786@gmail.com>
2026-07-16 11:55:10 +0530

    Reflection

---
+++ 10_Reflection/src/Eagle.java
@@ -0,0 +1,12 @@
+public class Eagle {
+    public  String breed;
+    private boolean canSwim;
+    public void fly(int intParam, boolean booleanParam, String stringParam){
+        System.out.println("Fly: IntParam:"+intParam+" BooleanParam: "+booleanParam+"String Param: "+stringParam);
+    }
+    public void eat(){
+        System.out.println("Eat");
+    }
+
+    public Eagle(){}
+}
---
+++ 10_Reflection/src/Sparrow.java
@@ -0,0 +1,6 @@
+public class Sparrow {
+    private Sparrow(){}
+    public void canFly(){
+        System.out.println("Sparrow can fly");
+    }
+}
